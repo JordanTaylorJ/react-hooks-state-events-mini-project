44bf504f72bc34e8e5c2493a9072841a1abeeffb
JordanJoseph <jordant.joseph@outlook.com>
2022-05-05 16:28:13 -0600

    commit tree 8b0c7c65cfdd79ce3908d9094c7784b6a57efe75

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import { CATEGORIES } from "../data";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, onSelectCategory}) {
+  const filteredCategories = CATEGORIES.filter(category => category.includes(selectedCategory))
+  const categoryButtons = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : null;
+    return <button
+      key={category}
+      className={className}
+      onClick={() => onSelectCategory(category)}
+    > 
+      {category} 
+    </button>
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
